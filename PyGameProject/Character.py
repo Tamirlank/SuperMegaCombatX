47ed1b4c23bf008ab71a0c24a0fe91a8478a9997
Tamirlank <34386460+Tamirlank@users.noreply.github.com>
2023-11-04 18:02:23 +0600

    Add files via upload

---
+++ PyGameProject/Character.py
@@ -0,0 +1,20 @@
+import pygame
+import Game
+class Character():
+    def __init__(self, x, y, name, max_hp, strength, sprite, scale):
+        self.x = x
+        self.y = y
+        self.name = name
+        self.max_hp = max_hp
+        self.hp = max_hp
+        self.strength = strength
+        self.alive = True
+        self.image = pygame.image.load(sprite)
+        self.image = pygame.transform.scale_by(self.image, scale)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        #self.image = pygame.transform.scale_by(self.image, )
+        
+    def draw(self):
+        Game.screen.blit(self.image, self.rect)
+        #pygame.draw.circle(screen, (0, 255, 0), (self.x, self.y), 30)
---
+++ PyGameProject/Game.py
@@ -0,0 +1,64 @@
+import pygame
+import sys
+import os
+import Character
+import UI
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screenWidth = 960
+screenHeight = 540
+
+screen = pygame.display.set_mode((screenWidth, screenHeight + 150))
+
+font = pygame.font.SysFont('Arial', 26)
+
+red = (255, 0, 0)
+green = (0, 255, 0)
+
+bg = pygame.image.load(os.path.join("./", "background.png"))
+bg = pygame.transform.scale(bg, (screenWidth, screenHeight))
+
+pygame.display.set_caption('Super Combat X')
+
+#dexterity, luck, defense, magic_str
+
+
+cloud = Character(200, 250, 'Cloud', 30, 10, 'cloud.jpg', 0.5)
+slime = Character(700, 250, 'Slime', 30, 10, 'Slime.png', 0.25)
+
+def draw_bg():
+    screen.blit(bg, (0, 0))
+
+def draw_characters():
+    cloud.draw()
+    slime.draw()
+
+initiative = [cloud, slime]
+turn = 0
+
+while True:
+    clock.tick(60)
+    x, y = pygame.mouse.get_pos()
+
+    #render stuff
+    draw_bg()
+    draw_characters()
+    UI.draw_chevron()
+    
+    UI.draw_text(f'{cloud.name} HP: {cloud.hp}', font, green, 200, 120)
+
+    #close the game if it's... closed
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                turn += 1
+                if (turn > len(initiative) - 1):
+                    turn = 0
+
+    #update game state
+    pygame.display.update()
